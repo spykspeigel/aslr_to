b30f79b8347fa71ff819110bfb789fa430b04fd4
spykspeigel <chhatoisaroj@gmail.com>
2021-12-03 18:10:53 +0530

    [residual] small comment

--- python/aslr_to/residual_frame_placement.py
+++ python/aslr_to/residual_frame_placement.py
@@ -2,6 +2,8 @@ import numpy as np
 import pinocchio
 import crocoddyl
 
+
+#Currently only supports Fully actuated case
 class ResidualModelFramePlacementASR(crocoddyl.ResidualModelAbstract):
     def __init__(self, state, frame_id=None, placement=None, nu=None):
         crocoddyl.ResidualModelAbstract.__init__(self, state, 6, nu,True, False, False)
